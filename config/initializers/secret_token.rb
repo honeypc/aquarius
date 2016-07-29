193652f6743a0babeeea85e73fdaf3c19e2ca0b1
Phuoc Tu <tuquangphuoc@gmail.com>
2016-07-29 09:46:35 +0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aquarius::Application.config.secret_token = '9d24d21de548348c0f55a9bc322de0984d5c24e9f3fa6000db8192f4012c837899bb4b5731288e4e7d06f5b1da6df655d4203201ba70810ba312983eed55532f'
